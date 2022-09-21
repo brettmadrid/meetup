b9400f1e66749a73769b6807f650ac4d2f43cacc
brettmadrid <brettmadrid9@gmail.com>
2022-09-21 14:14:49 -0700

    startup code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetups';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
