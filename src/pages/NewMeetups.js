c588daeed66722f313d4b3695354cbf8bd1a32f7
brettmadrid <brettmadrid9@gmail.com>
2022-09-26 18:25:36 -0700

    navigate away after form post

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupsPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       'https://react-getting-started-f15a5-default-rtdb.firebaseio.com/meetups.json',
@@ -11,7 +14,9 @@ function NewMeetupsPage() {
           'Content-Type': 'application/json',
         },
       },
-    );
+    ).then(() => {
+      history.replace('/');
+    });
   }
 
   return (
