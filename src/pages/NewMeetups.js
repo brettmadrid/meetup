af86a5ba1db8c0bb13af372f825f45620bab0db1
brettmadrid <brettmadrid9@gmail.com>
2022-09-26 17:46:34 -0700

    set up firebase database to receive data

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,25 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupsPage() {
-  return <div>New meetups page</div>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-f15a5-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
